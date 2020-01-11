a441ee5371fa2b1bdeba2a0d6c2577720c91cb6d
Mohini Thakur <59048563+Mohinithakur@users.noreply.github.com>
2020-01-11 13:09:49 +0530

    perform conditions and store in a list

---
+++ reboot_3.py
@@ -0,0 +1,31 @@
+list = []
+list1 =[]
+list2 = []
+
+n = int(input("Enter no.of elements : "))
+
+#taking user input.
+for i in range(0,n):
+    elements = int(input())
+
+    list.append(elements)
+print(list)
+
+print("Nos. greater than 5: ")
+#only those numbers greater than 5
+for i in range(0,n):
+    if (list[i]>5):
+        list1.append(list[i]) #storing the values in a list
+print(list1)
+
+print("Nos. less than 2: ")
+
+# those numbers those are less than or  equals to 2 
+for i in range(0,n):
+    if(list[i]<=2):
+        list2.append(list[i])#storing value in a list
+print(list2)
+
+
+
+
